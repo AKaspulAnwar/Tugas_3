ba5fa77d741b59f180151c0d2f0b835154569288
AKaspulAnwar <32286210+AKaspulAnwar@users.noreply.github.com>
2017-11-08 06:13:33 +0800

    perbaikan

--- no3.cpp
+++ no3.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main(){
 	int gape,jake,lembur,gatot,pajak ;
 	
-	cout << " Masukan gaji pegawai : ";
+	cout << " Input gaji pegawai : ";
 	cin >> gape;
-	cout << " Masukan jam kerja : ";
+	cout << " Input jam kerja : ";
 	cin >> jake;
 	
 	lembur = jake - 40;
