ca4aff1ae452ef52897c9ea6c09ac51c2880f647
AKaspulAnwar <32286210+AKaspulAnwar@users.noreply.github.com>
2017-11-08 19:23:29 +0800

    biar bagus

--- no3.cpp
+++ no3.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main(){
 	int gape,jake,lembur,gatot,pajak,gabe ;
 	
-	cout << " Input gaji pegawai : ";
+	cout << " Input gaji pegawai	: ";
 	cin >> gape;
-	cout << " Input jam kerja : ";
+	cout << " Input jam kerja	: ";
 	cin >> jake;
 	
 	lembur = (jake - 40)*(gape / 40);
@@ -16,8 +16,8 @@ int main(){
 	pajak = gatot*0.15;
 	gabe = gatot-pajak;
 	
-	cout << " Lemburan : "<< lembur << endl ;
-	cout << " Total  gaji : "<< gatot << endl;
-	cout << " Potongan pajak 15% : "<< pajak << endl;
-	cout << " Gaji bersih pegawai : "<< gabe << endl;
+	cout << " Lemburan 		: "<< lembur << endl ;
+	cout << " Total  gaji		: "<< gatot << endl;
+	cout << " Potongan pajak 15%	: "<< pajak << endl;
+	cout << " Gaji bersih pegawai	: "<< gabe << endl;
 }
